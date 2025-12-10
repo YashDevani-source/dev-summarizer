a62f11377f0f477479bd9a615cf1ed1dc428b480
Yash Devani <yashdewani159@gmail.com>
2025-12-11 03:02:01 +0530

    feat: add CI pipeline (closes #1)

--- app/server.js
+++ app/server.js
@@ -1,13 +1,13 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/health' && req.method === 'GET'){
-        res.writeHead(200, { 'Content-Type': 'application/json'})
-        res.end(JSON.stringify({status: 'OK'}));
-    } else{
-        res.writeHead(404);
-        res.end();
-    }
+  if (req.url === "/health" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ status: "OK" }));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
 const PORT = process.env.PORT || 4000;
